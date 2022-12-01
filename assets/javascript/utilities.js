36c8970b731e5e1ff7cf77a5148924c9d42cb745
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2022-12-01 17:43:32 +0100

    Button Erase with clearInterval

---
+++ assets/javascript/utilities.js
@@ -0,0 +1,58 @@
+const myButtonsPlus = document.querySelector(".button.buttonPlus"); //select button plus +
+const myButtonsMinus = document.querySelector(".button.buttonMinus"); //select button minus -
+const myButtonsErase = document.querySelector(".button.buttonErase"); //select button Erase
+const myButtonsHelp = document.querySelector(".button.buttonHelp"); //select button Help
+let helpingIntervalToStartCounting = null;
+let conditionClickAgain=true;
+
+
+function plusNumberInput(myButtonsPlus){
+    myButtonsPlus.addEventListener("click", ()=> {
+        updateInput(1);
+    });
+}
+
+function minusNumberInput(myButtonsMinus){
+    myButtonsMinus.addEventListener("click", ()=>  {
+        updateInput(-1);
+    });
+}
+
+function eraseInput(myButtonsErase){
+    myButtonsErase.addEventListener("click", ()=>  {
+        clearInterval(helpingIntervalToStartCounting);
+        updateInput(0);
+    });
+}
+
+function helpInput(myButtonsHelp){
+    myButtonsHelp.addEventListener("click", ()=>  {
+        conditionClickAgain=!conditionClickAgain;
+        if(conditionClickAgain==false){
+            helpingIntervalToStartCounting= setInterval(function() {
+                updateInput(1);
+                console.log("1");
+              }, 1000);  
+        }else{
+            console.log("I'M in the stop condition");
+            clearInterval(helpingIntervalToStartCounting);
+        }
+    });
+}
+
+function updateInput(number){
+    if(number==0){
+        document.querySelector(".input").value= //value update to zero
+        '';
+    }else{
+        let valueOfCounter = Number(document.querySelector(".input").value); // select old value of Input and make sure we work with number
+        document.querySelector(".input").value= //value update
+        valueOfCounter + number;
+    }
+
+}
+
+plusNumberInput(myButtonsPlus);
+minusNumberInput(myButtonsMinus);
+eraseInput(myButtonsErase);
+helpInput(myButtonsHelp);
