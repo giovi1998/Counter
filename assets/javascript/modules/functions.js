6d8ced066499386e0ea6306f4070bc51cd31db90
Giovanni Cidu <46072033+giovi1998@users.noreply.github.com>
2022-12-05 20:52:12 +0100

    Division of More JS FILE
    createElementsHTML -> Funtions that create the elements
    
    funtions -> Logic of the button

---
+++ assets/javascript/modules/functions.js
@@ -0,0 +1,50 @@
+export let helpingIntervalToStartCounting = null;
+export let conditionClickAgain=true;
+
+export function plusNumberInput(myButtonsPlus){
+    myButtonsPlus.addEventListener("click", ()=> {
+        updateInput(1);
+    });
+}
+
+export function minusNumberInput(myButtonsMinus){
+    myButtonsMinus.addEventListener("click", ()=>  {
+        updateInput(-1);
+    });
+}
+
+export function eraseInput(myButtonsErase){
+    myButtonsErase.addEventListener("click", ()=>  {
+        clearInterval(helpingIntervalToStartCounting);
+        updateInput(0);
+    });
+}
+
+export function helpInput(myButtonsHelp){
+    myButtonsHelp.addEventListener("click", ()=>  {
+        conditionClickAgain=!conditionClickAgain;
+        if(conditionClickAgain==false){
+            helpingIntervalToStartCounting= setInterval(function() {
+                updateInput(1);
+                console.log("1");
+              }, 1000);  
+        }else{
+            console.log("I'M in the stop condition");
+            clearInterval(helpingIntervalToStartCounting);
+        }
+    });
+}
+
+export function updateInput(number){
+    if(number==0){
+        document.querySelector(".input").value= //value update to zero
+        '';
+    }else{
+        let valueOfCounter = Number(document.querySelector(".input").value); // select old value of Input and make sure we work with number
+        document.querySelector(".input").value= //value update
+        valueOfCounter + number;
+    }
+
+}
+
+
